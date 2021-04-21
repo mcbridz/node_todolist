ff7134da8c3530c4b2d57073d008f0ed319402d9
mcbridz@gmail.com <mcbridz@gmail.com>
2021-04-20 18:36:35 -0700

    periodic

--- site_script.js
+++ site_script.js
@@ -1,14 +1,7 @@
 //set event listenters that use functions for things
 //start by setting up on-site load of existing todos
+const spa = require('./utils/spa')
 
-let data = [
-    // {
-    // 'todo': 'abc',
-    // 'priority': 'low',
-    // 'dateAdded': '4/21/21',
-    // 'completed': false
-    // }
-]
 let todo_items = document.querySelector("#todo_items")
 let body = document.querySelector('body')
 // function getTodos() {
@@ -25,69 +18,9 @@ let body = document.querySelector('body')
 // }
 
 
-function mark_complete(index) {
-    return () => {
-        if (data[index].completed) {
-            data[index].completed = false
-            data[index].completed_str = "false"
-        } else {
-            data[index].completed = true
-            data[index].completed_str = "true"
 
-        }
-        // (data[index].completed) ? data[index].completed = false : data[index].completed = true
-        display_todos()
-    }
-}
-function display_todos() {
-    todo_items.innerHTML = ""
-    console.log("current data is:")
-    console.log(data)
-    for (let i = 0; i < data.length; ++i) {
-        let newDiv = document.createElement("div")
-        newDiv.id = `todo${i}`
-        let newTodo = {
-            'todo': data[i].todo,
-            'priority': data[i].priority,
-            'dateAdded': data[i].dateAdded,
-            'completed': data[i].completed,
-            'completed_str': data[i].completed_str
-        }
-        let todo_span = document.createElement("span")
-        let priority_span = document.createElement("span")
-        let date_span = document.createElement("span")
-        let completed_span = document.createElement("span")
-        let completed_btn = document.createElement("button")
-        todo_span.innerText = newTodo.todo
-        priority_span.innerText = newTodo.priority
-        date_span.innerText = newTodo.dateAdded
-        completed_span.innerText = newTodo.completed_str
-        completed_btn.innerText = "Mark (un)complete"
-        completed_btn.onclick = mark_complete(i)
-        newDiv.appendChild(todo_span)
-        newDiv.appendChild(priority_span)
-        newDiv.appendChild(date_span)
-        newDiv.appendChild(completed_span)
-        newDiv.appendChild(completed_btn)
-        todo_items.appendChild(newDiv)
-    }
-}
 
 
 let submit = document.querySelector('#submit')
-function newTodo(callback) {
-    console.log('button pushed!')
-    let todo = document.querySelector('#input').value
-    let priority = document.querySelector('#input_priority').value
-    let date = new Date()
-    date = `${date.getDate().toString()}/${(date.getMonth() + 1).toString()}/${(date.getFullYear() % 2000).toString()}`
-    data.push({
-        'todo': todo,
-        'priority': priority,
-        'dateAdded': date,
-        'completed_str': 'False',
-        'completed': false
-    })
-    callback()
-}
-body.onload = display_todos()
+submit.onclick = () => function () { spa.newTodo(spa.display_todos()) }
+body.onload = spa.display_todos()
---
+++ utils/spa.js
@@ -0,0 +1,77 @@
+function mark_complete(index) {
+    return () => {
+        let todo_store = document.querySelector('#todosJSON')
+        let todosJSON = JSON.parse(todo_store.innerText)
+        if (todosJSON[index].completed) {
+            todosJSON[index].completed = false
+            todosJSON[index].completed_str = "false"
+        } else {
+            todosJSON[index].completed = true
+            todosJSON[index].completed_str = "true"
+        }
+        todo_store.innerText = JSON.stringify(todosJSON)
+        display_todos()
+    }
+}
+function display_todos() {
+    let todo_items = document.querySelector("#todo_items")
+    let todosJSON = JSON.parse(document.querySelector('#todosJSON').innerText)
+    todo_items.innerText = ""
+    console.log("current data is:")
+    console.log(todosJSON)
+    for (let i = 0; i < todosJSON.length; ++i) {
+        let newDiv = document.createElement("div")
+        newDiv.id = `todo${i}`
+        let newTodo = {
+            'todo': todosJSON[i].todo,
+            'priority': todosJSON[i].priority,
+            'dateAdded': todosJSON[i].dateAdded,
+            'completed': todosJSON[i].completed,
+            'completed_str': todosJSON[i].completed_str
+        }
+        let todo_span = document.createElement("span")
+        let priority_span = document.createElement("span")
+        let date_span = document.createElement("span")
+        let completed_span = document.createElement("span")
+        let completed_btn = document.createElement("button")
+        todo_span.innerText = newTodo.todo
+        priority_span.innerText = newTodo.priority
+        date_span.innerText = newTodo.dateAdded
+        completed_span.innerText = newTodo.completed_str
+        completed_btn.innerText = "Mark (un)complete"
+        completed_btn.onclick = mark_complete(i)
+        newDiv.appendChild(todo_span)
+        newDiv.appendChild(priority_span)
+        newDiv.appendChild(date_span)
+        newDiv.appendChild(completed_span)
+        newDiv.appendChild(completed_btn)
+        todo_items.appendChild(newDiv)
+    }
+}
+function newTodo(callback) {
+    console.log('button pushed!')
+    let todo_store = document.querySelector('#todosJSON')
+    let todosJSON = JSON.parse(todo_store.innerText)
+    let todo = document.querySelector('#input').value
+    let priority = document.querySelector('#input_priority').value
+    let date = new Date()
+    date = `${(date.getMonth() + 1).toString()}/${date.getDate().toString()}/${(date.getFullYear() % 2000).toString()}`
+    todosJSON.push({
+        'todo': todo,
+        'priority': priority,
+        'dateAdded': date,
+        'completed_str': 'False',
+        'completed': false
+    })
+    todo_store.innerText = JSON.stringify(todosJSON)
+    callback()
+}
+let data = [
+    // {
+    // 'todo': 'abc',
+    // 'priority': 'low',
+    // 'dateAdded': '4/21/21',
+    // 'completed': false
+    // }
+]
+module.exports = { data, newTodo, display_todos, mark_complete }
